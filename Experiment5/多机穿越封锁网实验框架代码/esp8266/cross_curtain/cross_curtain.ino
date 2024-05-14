90442be60356369b33c90934f0e4d64970e987c8
Skipper <chanandlerbongwch@gmail.com>
2024-05-14 15:00:51 +0800

    delete submodule

---
+++ Experiment5/多机穿越封锁网实验框架代码/esp8266/cross_curtain/cross_curtain.ino
@@ -0,0 +1,251 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <Arduino.h>
+#include <WiFiUdp.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "cmath"
+#include "stdarg.h"
+
+#define STASSID "HUAWEI"
+#define STAPSK "a123456789"
+
+#define UDP_PRINT_IP "192.168.31.91"
+#define UDP_PRINT_PORT 59998
+
+#define UDP_BUFFER_SIZE 1024
+
+unsigned int localPort = 8888;
+char packetBuffer[UDP_BUFFER_SIZE];  // buffer to hold incoming packet,
+WiFiUDP Udp;
+
+#define LED_PIN (2)
+
+float *forward(float *);
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+
+ESP8266WiFiMulti WiFiMulti;
+
+uint8_t rxByte = 0;
+uint8_t cksum = 0;
+
+typedef union _cmd_req
+{
+    struct
+    {
+        uint8_t req;
+        float vx;
+        float vy;
+        float vz;
+    };
+    uint8_t buf[13];
+} __attribute__((packed)) cmd_req;
+
+cmd_req cmdreq;
+
+typedef union _v_data
+{
+    struct
+    {
+        float vx;
+        float vy;
+        float vz;
+        uint32_t land;
+        float vx_r;
+        float vy_r;
+        float vz_r;
+        float w_r;
+        float theta;
+    };
+    uint8_t buf[36];
+} __attribute__((packed)) v_data;
+
+v_data vsetpoint;
+
+uint8_t checksum(const uint32_t size, uint8_t *packet)
+{
+    uint8_t cksum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        cksum += packet[i];
+    }
+    return cksum;
+}
+
+int32_t uart_read(const uint32_t size, uint8_t *data)
+{
+    Serial.readBytes(data, size);
+    Serial.readBytes(&rxByte, 1);
+
+    if (checksum(size, data) != rxByte)
+    {
+        // printf("Uart read: checksum dismatch !\n");
+        return 0;
+    }
+    return 1;
+}
+
+void uart_write(const uint32_t size, uint8_t *data)
+{
+    cksum = checksum(size, data);
+    Serial.write(data, size);
+    Serial.write(&cksum, 1);
+}
+
+void udp_send(const char *ip, uint16_t port, const char *buffer, size_t size) {
+    IPAddress ipaddr;
+    ipaddr.fromString(ip);
+    Udp.beginPacket(ipaddr, port);
+    Udp.write(buffer, size);
+    Udp.endPacket();
+}
+
+// void udp_print(const char *fmt, ...) {
+//     static char udp_print_buffer[64];
+
+//     va_list args;
+//     va_start(args, fmt);
+
+//     vsprintf(udp_print_buffer, fmt, args);
+//     udp_send(UDP_PRINT_IP, UDP_PRINT_PORT, udp_print_buffer, strlen(udp_print_buffer) + 1);
+
+//     va_end(args);
+// }
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);  // LED ON
+
+    Serial.setTimeout(0x0fffffff);
+    Serial.begin(115200);
+    while (!Serial)
+        ;
+    //  digitalWrite(LED_PIN, LOW);
+
+    // We start by connecting to a WiFi network
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP(ssid, password);
+
+    Serial.println();
+    Serial.println();
+    Serial.print("[ESP8266]: Wait for WiFi... ");
+
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("");
+    Serial.println("[ESP8266]: WiFi connected");
+    Serial.println("[ESP8266]: IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Udp.begin(localPort);
+    delay(500);
+}
+
+#define OBS_DIM (11)
+#define MAX_DZ (0.1f)
+
+typedef struct _info_type
+{
+    float obs[OBS_DIM];
+    float p_k;
+    float vel;
+    float z;
+} info_type;
+
+info_type recv_info;
+
+uint32_t step_cnt = 0;
+static uint8_t buffer[64];
+void loop()
+{
+    const float dt = 0.2;
+    int packetSize = Udp.parsePacket();
+    if (packetSize == 0) {
+        return;
+    }
+
+    // read the packet into packetBufffer
+    int n = Udp.read(packetBuffer, UDP_BUFFER_SIZE);
+    packetBuffer[n] = 0;
+    if (n == sizeof(info_type)) {
+        memcpy((void *)(&recv_info), packetBuffer, sizeof(recv_info));
+    } else {
+        // send a reply, to the IP address and port that sent us the packet we received
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write("ERROR_SIZE");
+        Udp.endPacket();
+        return;
+    }
+
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+
+    float input[OBS_DIM];
+    for (int i = 0; i < OBS_DIM; ++i)
+    {
+        input[i] = recv_info.obs[i];
+    }
+
+    float now_z = recv_info.z; // get current height
+    float abs_vel = recv_info.vel; // get absolute velocity
+    float k_p = recv_info.p_k; // get K_P
+
+    uart_read(sizeof(cmdreq.buf), cmdreq.buf);
+    float theta_real = atan2(cmdreq.vy, cmdreq.vx); // read theta from the flight controller
+    input[2] = theta_real;
+    float *result = forward(input);
+
+    float target_z = now_z + result[1] * MAX_DZ * dt;
+    if (target_z < 0.45) target_z = 0.45;
+
+    if (cmdreq.buf[0] != 0x66)
+    {
+        return;
+    }
+
+    float d_theta = result[0];
+    float vxy = abs_vel;
+    float theta = theta_real + d_theta * dt;
+    
+    
+    if (step_cnt < 3) {
+        theta = M_PI / 2;
+    }
+    step_cnt += 1;
+
+    if (theta > M_PI)
+        theta -= 2 * M_PI;
+    else if (theta < -M_PI)
+        theta += 2 * M_PI;
+
+    float vx = vxy * cos(theta);
+    float vy = vxy * sin(theta);
+    float vz = (target_z - now_z) * k_p;
+
+    float dist2tar2 = input[0] * input[0] + input[1] * input[1];
+    uint32_t yaw_rate = -1;
+
+    uint32_t land = 0;
+    if (dist2tar2 < 0.5 * 0.5) {
+        land = 1;
+    }
+
+    vsetpoint.vx = vx;
+    vsetpoint.vy = vy;
+    vsetpoint.vz = vz;
+    vsetpoint.land = land;
+    vsetpoint.vx_r = cmdreq.vx;
+    vsetpoint.vy_r = cmdreq.vy;
+    vsetpoint.vz_r = cmdreq.vz;
+    vsetpoint.w_r = theta_real;
+    vsetpoint.theta = theta;
+
+    uart_write(sizeof(vsetpoint.buf), vsetpoint.buf);
+}
---
+++ Experiment5/多机穿越封锁网实验框架代码/esp8266/cross_curtain/pytorch2c.py
@@ -0,0 +1,123 @@
+import torch
+from torch import nn
+import sys
+
+class Net(nn.Module):
+
+    def __init__(self, obs_dim, action_dim):
+        super().__init__()
+        self.fc0 = torch.nn.Linear(obs_dim, 64)
+        self.fc1 = torch.nn.Linear(64, 64)
+        self.fc2 = torch.nn.Linear(64, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc0(x))
+        x = torch.relu(self.fc1(x))
+        a = torch.tanh(self.fc2(x))
+        return a
+
+
+net = Net(11, 2)
+policy_state_dict = torch.load(sys.argv[1])
+net.load_state_dict(policy_state_dict)
+state_dict = net.state_dict()
+
+
+def save_model_as_c(model: torch.nn, activation: list, name: str="forward"):
+    state_dict = model.state_dict()
+    n_layers = len(state_dict) // 2
+    codes = [f'static int n = {n_layers};\n', f'static int weights_shape[] = {{']
+    activation_str = str(activation).strip('[]').replace('\'', '"')
+    codes.append(f'static const char *act_funcs[] = {{{activation_str}}};\n')
+    weights = ''
+    bias = ''
+    weights_shape = []
+    for k, v in state_dict.items():
+        tmp = ''
+        for s in v.reshape(-1):
+            tmp += f'{float(s):f} '
+        if k.split('.')[1] == 'weight':
+            codes[1] += f'{tuple(v.shape)}'.strip('()') + ', '
+            weights += tmp
+            weights_shape.append(tuple(v.shape))
+        else:
+            bias += tmp
+    weights = weights.strip().replace(' ', ', ')
+    codes.append(f'static float __weights_1d[] = {{{weights}}};\n')
+    bias = bias.strip().replace(' ', ', ')
+    codes.append(f'static float __bias_1d[] = {{{bias}}};\n')
+    codes[1] = codes[1].strip(' ,') + '};\n'
+
+    start_index = 0
+    for i in range(n_layers):
+        row, col = weights_shape[i]
+        init_st = ''
+        for j in range(row):
+            init_st += f'&__weights_1d[{start_index + j * col}], '
+        start_index += row * col
+        init_st = '{' + init_st.strip(', ') + '}'
+        codes.append(f'static float *__w{i}[{row}] = {init_st};\n')
+
+    init_st = ''
+    for i in range(n_layers):
+        init_st += f'__w{i}, '
+    init_st = '{' + init_st.strip(', ') + '}'
+
+    codes.append(f'static float **weights[{n_layers}] = {init_st};\n')
+
+    init_st = ''
+    start_index = 0
+    for i in range(n_layers):
+        row, col = weights_shape[i]
+        init_st += f'&__bias_1d[{start_index}], '
+        start_index += row
+    init_st = '{' + init_st.strip(', ') + '}'
+    codes.append(f'static float *bias[{n_layers}] = {init_st};\n')
+
+    max_output_dim = max([x[0] for x in weights_shape])
+    codes.append(f'static float inter_result[{max_output_dim}];\n')
+    codes.append(f'static float prev_output[{max_output_dim}];\n')
+    codes.append(f'static float output[{weights_shape[-1][0]}];\n')
+
+    codes.append(f"""
+void relu(int col, float *input);
+void tanh_(int col, float *input);
+void matmul_1d(int row, int col, float *input, float **weights, float *output);
+void add_bias(int col, float *input, float *bias);
+void activate(int col, float *input, const char *act_fun);
+
+float *{name}(float *input) {{
+    int row = weights_shape[0], col = weights_shape[1];
+    matmul_1d(row, col, input, weights[0], prev_output);
+    add_bias(row, prev_output, bias[0]);
+    activate(row, prev_output, act_funcs[0]);
+    
+    for (int i = 1; i < n; i++) {{
+        int row = weights_shape[2 * i], col = weights_shape[2 * i + 1];
+
+        matmul_1d(row, col, prev_output, weights[i], inter_result);
+        add_bias(row, inter_result, bias[i]);
+        activate(row, inter_result, act_funcs[i]);
+
+        for (int j = 0; j < row; j++) {{
+            prev_output[j] = inter_result[j];
+        }}
+    }}
+
+    for(int i = 0; i < weights_shape[2 * (n - 1)]; i++) {{
+        output[i] = prev_output[i];
+    }}
+
+    return output;
+}}""")
+
+    with open(f"{name}.cpp", 'w') as f:
+        f.writelines(codes)
+
+    total_para_num = 0
+    for ws in weights_shape:
+        total_para_num += ws[0] * ws[1] + ws[0]
+    print(f'Finish saving, total number of parameters: {total_para_num}')
+
+
+save_model_as_c(net, ['relu', 'relu', 'tanh'])
